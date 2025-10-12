86b95563e8660ac534bcf08a84f28fc952ffbcd0
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-12 11:23:58 +0200

    creation of single picture component

---
+++ src/app/core/components/single-picture/single-picture.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-picture',
+  templateUrl: './single-picture.component.html',
+  styleUrl: './single-picture.component.css'
+})
+export class SinglePictureComponent {
+
+}
