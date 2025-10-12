d9a48f4095ba6c8690dfecdaddaf7e3a7977bb01
ZaeRon007 <julien.pelissier8@orange.fr>
2025-10-12 23:40:12 +0200

    single picture component update

--- src/app/core/components/single-picture/single-picture.component.ts
+++ src/app/core/components/single-picture/single-picture.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-single-picture',
   templateUrl: './single-picture.component.html',
   styleUrl: './single-picture.component.css'
 })
-export class SinglePictureComponent {
+export class SinglePictureComponent implements OnInit{
 
+  Name!: string;
+
+  constructor(private route: ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+    this.Name = this.route.snapshot.paramMap.get('name') ?? '';
+    console.log(this.Name);
+  }
 }
